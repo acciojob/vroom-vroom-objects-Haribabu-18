4d92aa0df981d9433853429fd718a7dcaff4305b
HARI BABU ANAKAPALLI <142531911+Haribabu-18@users.noreply.github.com>
2026-01-28 16:24:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,34 @@
 // Complete the js code
-function Car(make, model) {}
+function Car(make, model) {
+	this.make = make;
+	this.model = model;
+}
 
-function SportsCar(make, model, topSpeed) {}
+Car.prototype.getMakeModel = function () {
+	return `${this.make} ${this.model}`;
+};
+
+function SportsCar(make, model, topSpeed) {
+	Car.call(this, make, model);
+	this.speed = topSpeed;
+}
+
+// Proper inheritance
+SportsCar.prototype = Object.create(Car.prototype);
+SportsCar.prototype.constructor = SportsCar;
+
+SportsCar.prototype.getTopSpeed = function () {
+	return this.speed;
+};
+
+// Create a new SportsCar instance
+const car = new SportsCar("Ferrari", "Testarossa", 200);
+
+// Call getMakeModel() to get the make and model of the car
+console.log(car.getMakeModel()); // Ferrari Testarossa
+
+// Call getTopSpeed() to get the top speed of the sports car
+console.log(car.getTopSpeed()); // 200
 
 // Do not change the code below
 window.Car = Car;
